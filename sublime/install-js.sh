008267d0b7cefdc8cb6ed22609917201c47d494f
Xianming Zhong <chinesedfan@qq.com>
2023-07-24 22:53:16 +0800

    fix install-js.sh

--- sublime/install-js.sh
+++ sublime/install-js.sh
@@ -7,14 +7,14 @@ mkdir -p "$PACKAGES_DIR"
 
 cd "$PACKAGES_DIR"
 git init
-git remote add git@github.com:chinesedfan/Packages.git
+git remote add origin git@github.com:chinesedfan/Packages.git
 git config core.sparsecheckout true
 echo "JavaScript/*.sublime-snippet" >> .git/info/sparse-checkout
 git pull origin master
 
 echo '...git done!'
 
-mkdir "$SUBLIME_JS_DIR"
-ln -sf ${PACKAGES_DIR} "${SUBLIME_JS_DIR}/Snippets"
+mkdir -p "$SUBLIME_JS_DIR"
+ln -sf "${PACKAGES_DIR}/JavaScript" "${SUBLIME_JS_DIR}/Snippets"
 
 echo '...ln done!'
